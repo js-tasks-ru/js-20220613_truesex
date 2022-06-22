02150b63a84d5a505eab75a660c4fbf4ab31ac3d
AK <pro100wow@gmail.com>
2022-06-23 02:55:20 +0300

    part 3 of project done

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,28 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const arr = path.split('.');
+  return (obj) => {
+    let res = obj;
+    arr.every(value => {
+      try {
+        res = res[value];
+        console.log(`${value} ${res}`);
+      } catch (error) {
+        res = undefined; //может к примитивному типу обратиться по свойству
+      }
+      return res !== undefined;
+    });
+    return res;
+  }
 }
+/*
+const product = {
+  category: {
+    title: "Goods"
+  }
+}
+
+const getter = createGetter('category.title');
+
+console.log('' + getter(product) + ' ' + product); // Goods*/
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,10 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+    const set = new Set(arr);
+    return [...set];
 }
+
+console.log(uniq([1, 2, 2, 3, 1, 4])); // [1, 2, 3, 4]
+console.log(uniq([]));
+console.log(uniq());
